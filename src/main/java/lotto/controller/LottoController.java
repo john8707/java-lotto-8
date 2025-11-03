d63eb72576f3b173711aa38e4fe4adef63cb8406
john8707 <doobgyul@gmail.com>
2025-11-03 18:32:17 +0900

    feat(view): add printing purchased lotto numbers

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,20 +2,30 @@ package lotto.controller;
 
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.List;
+
 import lotto.domain.PurchaseAmount;
+import lotto.domain.Lotto;
+import lotto.service.LottoService;
 
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final LottoService lottoService;
     
     public LottoController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.lottoService = new LottoService();
     }
 
     public void run() {
         PurchaseAmount purchaseAmount = getPurchaseAmount();
         outputView.printLottoCount(purchaseAmount.getLottoCount());
+
+        List<Lotto> lottos = lottoService.generateLottoNumbers(purchaseAmount.getLottoCount());
+        outputView.printLottoNumbers(lottos);
     }
 
     private PurchaseAmount getPurchaseAmount() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,18 @@
 package lotto.view;
 
+import java.util.List;
+
+import lotto.domain.Lotto;
+
 public class OutputView {
     private static final String OUTPUT_LOTTO_COUNT = "%d개를 구매했습니다.";
     public void printLottoCount(int count) {
         System.out.printf(OUTPUT_LOTTO_COUNT + "\n", count);
+    }
+
+    public void printLottoNumbers(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
