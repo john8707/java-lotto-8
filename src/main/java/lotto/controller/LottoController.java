14fdc84b422572618a9d167abd4cd8cc95c5f117
john8707 <doobgyul@gmail.com>
2025-11-03 20:50:11 +0900

    feat: get winning lotto numbers from input

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,13 +1,14 @@
 package lotto.controller;
 
-import lotto.view.InputView;
-import lotto.view.OutputView;
-
 import java.util.List;
+import java.util.Arrays;
 
 import lotto.domain.PurchaseAmount;
 import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
 import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
     private final InputView inputView;
@@ -26,11 +27,26 @@ public class LottoController {
 
         List<Lotto> lottos = lottoService.generateLottoNumbers(purchaseAmount.getLottoCount());
         outputView.printLottoNumbers(lottos);
+
+        WinningLotto winningLotto = getWinningLotto();
     }
 
     private PurchaseAmount getPurchaseAmount() {
         String input = inputView.readPurchaseAmount();
         int amount = Integer.parseInt(input);
         return new PurchaseAmount(amount);
+    }
+
+    private WinningLotto getWinningLotto() {
+        String inputWinningNumbers = inputView.readWinningNumbers();
+        String inputBonusNumber = inputView.readBonusNumber();
+
+        List<Integer> winningNumbers = Arrays.stream(inputWinningNumbers.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+
+        int bonusNumber = Integer.parseInt(inputBonusNumber);
+        return new WinningLotto(winningNumbers, bonusNumber);
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     private String readInput(String message) {
         System.out.println(message);
@@ -12,6 +14,18 @@ public class InputView {
 
     public String readPurchaseAmount() {
         String input = readInput(INPUT_PURCHASE_AMOUNT);
+        System.out.println();
+        return input;
+    }
+
+    public String readWinningNumbers() {
+        String input = readInput(INPUT_WINNING_NUMBERS);
+        System.out.println();
+        return input;
+    }
+
+    public String readBonusNumber() {
+        String input = readInput(INPUT_BONUS_NUMBER);
         System.out.println();
         return input;
     }
