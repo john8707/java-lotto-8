fcc86d035dcec7e71c1c517241d8749d7ecbe96e
john8707 <doobgyul@gmail.com>
2025-11-03 17:35:54 +0900

    feat(domain): implement lotto purchase count getter

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,9 +1,14 @@
 package lotto.domain;
 
 public class PurchaseAmount {
+    private static final int LOTTO_PRICE = 1000;
     private final int amount;
 
     public PurchaseAmount(int amount) {
         this.amount = amount;
+    }
+
+    public int getLottoCount() {
+        return amount / LOTTO_PRICE;
     }
 }
